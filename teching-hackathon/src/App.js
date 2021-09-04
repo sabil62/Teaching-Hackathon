225a1ee3c3b9818445a2c764c1514e9827ad78e0
sabil62 <shresthasabil62@gmail.com>
2021-09-04 15:55:00 +0545

    assets included

--- teching-hackathon/src/App.js
+++ teching-hackathon/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <button className="btn btn-lg btn-success">lion</button>
     </div>
   );
 }
